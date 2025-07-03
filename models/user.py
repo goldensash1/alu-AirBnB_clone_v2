94b5875600c1bf355d4add9188aeae3c9d231168
goldensash1 <s.munyankin@alustudent.com>
2025-07-03 21:01:59 +0200

    read

--- models/user.py
+++ models/user.py
@@ -3,14 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
-        __tablename__: name of MySQL table
         email: email address
         password: password for login
         first_name: first name
@@ -23,7 +20,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user",
-                         cascade="all, delete-orphan")
-    reviews = relationship("Review", backref="user",
-                          cascade="all, delete-orphan")
+    places = relationship("Place", cascade="all, delete-orphan",
+                        backref="user")
+    reviews = relationship("Review", cascade="all, delete-orphan",
+                         backref="user")
