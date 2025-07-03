5d44a6b0195772b3b36b7c4a647b560d7094a181
goldensash1 <s.munyankin@alustudent.com>
2025-07-03 19:23:57 +0200

    read

--- models/state.py
+++ models/state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 import models
 from models.city import City
 import shlex
@@ -13,23 +12,27 @@ class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
+        cities: relationship to City
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+                         backref="state")
 
     @property
     def cities(self):
+        """Getter attribute that returns the list of City instances
+        with state_id equals to the current State.id
+        """
         var = models.storage.all()
         lista = []
         result = []
         for key in var:
             city = key.replace('.', ' ')
             city = shlex.split(city)
-            if (city[0] == 'City'):
+            if city[0] == 'City':
                 lista.append(var[key])
         for elem in lista:
-            if (elem.state_id == self.id):
+            if elem.state_id == self.id:
                 result.append(elem)
-        return (result)
+        return result
